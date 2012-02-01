229259e6aca7244fdcec5900d5ce0147546bcf05
Elliot <elliot@Ell.local>
2012-01-31 21:41:17 -0500

    Add time limit url

--- hello.py
+++ hello.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/timelimit")
+def time_limit():
+    return 'Only 54 hours total!'
+
 if __name__ == "__main__":
     app.run()
